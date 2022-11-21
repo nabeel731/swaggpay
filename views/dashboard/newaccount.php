12e4fd6bb19b12b3dd4c3563bccc37f11272d179
Nabeel Ahmad <nabeel53055@gmail.com>
2022-11-22 03:50:29 +0500

    work on admin

--- views/dashboard/newaccount.php
+++ views/dashboard/newaccount.php
@@ -156,7 +156,7 @@
         "paging": true,
         "lengthChange": true,
         "searching": true,
-        "ordering": true,
+        "ordering": false,
         "info": true,
         "autoWidth": true,
         "responsive": true,
